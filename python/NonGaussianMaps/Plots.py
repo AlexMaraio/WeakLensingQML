ad495be311bbfb7917ba83fb2edf0ac84d41a859
Alex Maraio <maraio@roe.ac.uk>
2022-05-31 12:57:56 +0100

    Update plots to include (optional) binning

--- python/NonGaussianMaps/Plots.py
+++ python/NonGaussianMaps/Plots.py
@@ -2,12 +2,16 @@
 Script that plots summary statistics (mean and standard deviation) for a set of Cl values that have been recovered
 from an ensemble of maps
 """
+
 import os
 import numpy as np
+import matplotlib as mpl
 from matplotlib import pyplot as plt
 import seaborn as sns
 from scipy import constants as consts
 import healpy as hp
+
+from Binning import Bins, BinType, uniform_weights, mode_count_weights, inverse_variance_weights
 
 
 sns.set(font_scale=1.25, rc={'text.usetex': True})
@@ -17,38 +21,40 @@ sns.set(font_scale=1.25, rc={'text.usetex': True})
 n_side = 256
 
 # The number of maps that were in the ensemble
-num_maps = 1250
+num_maps = 2_500
 
 # The location of map data
-folder_path = "/disk01/maraio/NonGaussianShear/N1024_LogNormal/Maps_N256"
+folder_path_gaussian = "/cephfs/maraio/NonGaussianMaps/N1024_Gaussian_shift_0_01214_whnoise/Maps_N256"
+folder_path_lognormal = "/cephfs/maraio/NonGaussianMaps/N1024_LogNormal_shift_0_01214_whnoise/Maps_N256"
 
 # The location of where to save the plots into
-plots_folder = "../data/Plots/N256_LogNormal"
+plots_folder = "../../data/Plots/LogNormalMaps/N256_comparison_binning"
 if not os.path.isdir(plots_folder):
     os.makedirs(plots_folder)
 
 # Compute resolution-associated quantities
-l_max = 2 * n_side
-l_max_full = 3 * n_side - 1
+l_max = 3 * n_side - 1
 ells = np.arange(2, l_max + 1)
-ells_full = np.arange(2, l_max_full + 1)
 n_ell = len(ells)
-n_ell_full = len(ells_full)
 
 # The polarisation HealPix window function values for the specified resolution
 pix_win = hp.pixwin(n_side, pol=True, lmax=l_max)[1][2:]
 
 # Read in the theory spectra associated with the maps
-ells, cl_th_EE = np.loadtxt(f'{folder_path}/../TheoryClsShear-f1z1f1z1.dat').T[:, 0: n_ell]
+ells, cl_th_EE = np.loadtxt(f'{folder_path_gaussian}/../TheoryClsShear-f1z1f1z1.dat').T[:, 0: n_ell]
+
+# Compute the expected variance of the shear maps
+cl_var = np.sum((2 * ells + 1) * cl_th_EE / (4 * np.pi) / 2 * (pix_win ** 2))
+print(f'Expected shear variance is {cl_var:.4e}')
 
 # Read in our QML Fisher matrix
-QML_fisher_filepath = f"../data/numerical_fishers/ConjGrad_Fisher_N{n_side}_nummaps10_noise3_EB_nostars.dat"
+QML_fisher_filepath = f"../../data/numerical_fishers/ConjGrad_Fisher_N{n_side}_nummaps25_noise3_EB_whstars.dat"
 
 # Read in the Fisher matrix and then remove EB entries, and make symmetric
 QML_fisher_matrix = np.loadtxt(QML_fisher_filepath)
 
-QML_fisher_matrix = np.delete(QML_fisher_matrix, np.s_[n_ell_full: 2 * n_ell_full], axis=0)
-QML_fisher_matrix = np.delete(QML_fisher_matrix, np.s_[n_ell_full: 2 * n_ell_full], axis=1)
+QML_fisher_matrix = np.delete(QML_fisher_matrix, np.s_[n_ell: 2 * n_ell], axis=0)
+QML_fisher_matrix = np.delete(QML_fisher_matrix, np.s_[n_ell: 2 * n_ell], axis=1)
 
 QML_fisher_matrix = (QML_fisher_matrix.copy() + QML_fisher_matrix.copy().T) / 2
 
@@ -60,108 +66,193 @@ intrinsic_gal_ellip = 0.21
 avg_gal_den = 3
 theory_cl_noise = intrinsic_gal_ellip ** 2 / (avg_gal_den / (consts.arcminute ** 2))
 
-# Create arrays where we will store our data into
-cl_samples_EE_QML = np.zeros([num_maps, len(ells_full)])
-cl_samples_BB_QML = np.zeros([num_maps, len(ells_full)])
+# Create bins object
+# Ells per bin of one corresponds to an unbinned spectra
+ells_per_bin = 1
+bins = Bins(n_side, BinType.linear, ells_per_bin, mode_count_weights)
 
-cl_samples_EE_PCl = np.zeros([num_maps, len(ells_full)])
-cl_samples_BB_PCl = np.zeros([num_maps, len(ells_full)])
+# Bin the theory vector
+cl_th_EE_binned = bins.bin_vector(cl_th_EE)
+
+# Create arrays where we will store our data into
+cl_samples_EE_QML_gaussian = np.zeros([num_maps, bins.num_bins])
+cl_samples_BB_QML_gaussian = np.zeros([num_maps, bins.num_bins])
+
+cl_samples_EE_PCl_gaussian = np.zeros([num_maps, bins.num_bins])
+cl_samples_BB_PCl_gaussian = np.zeros([num_maps, bins.num_bins])
+
+cl_samples_EE_QML_lognormal = np.zeros([num_maps, bins.num_bins])
+cl_samples_BB_QML_lognormal = np.zeros([num_maps, bins.num_bins])
+
+cl_samples_EE_PCl_lognormal = np.zeros([num_maps, bins.num_bins])
+cl_samples_BB_PCl_lognormal = np.zeros([num_maps, bins.num_bins])
 
 # Go through each map in our ensemble
 for map_num in range(num_maps):
-    # Load the QML power spectrum and store into QML data vectors
-    cl_EE_QML = np.loadtxt(f'{folder_path}/Map{map_num}_Cl_EE_QML.dat')
-    cl_BB_QML = np.loadtxt(f'{folder_path}/Map{map_num}_Cl_BB_QML.dat')
+    # Load the Gaussian QML power spectrum and store into QML data vectors
+    cl_EE_QML_gaussian = np.loadtxt(f'{folder_path_gaussian}/Map{map_num}_Cl_EE_QML.dat')
+    cl_BB_QML_gaussian = np.loadtxt(f'{folder_path_gaussian}/Map{map_num}_Cl_BB_QML.dat')
 
-    cl_samples_EE_QML[map_num - 1] = cl_EE_QML
-    cl_samples_BB_QML[map_num - 1] = cl_BB_QML
+    # Bin the power spectrum
+    cl_EE_QML_gaussian = bins.bin_vector(cl_EE_QML_gaussian)
+    cl_BB_QML_gaussian = bins.bin_vector(cl_BB_QML_gaussian)
 
-    # Load the PCl power spectrum and store into PCl data vectors
-    cl_EE_PCl = np.loadtxt(f'{folder_path}/Map{map_num}_Cl_EE_PCl.dat')
-    cl_BB_PCl = np.loadtxt(f'{folder_path}/Map{map_num}_Cl_BB_PCl.dat')
+    cl_samples_EE_QML_gaussian[map_num] = cl_EE_QML_gaussian
+    cl_samples_BB_QML_gaussian[map_num] = cl_BB_QML_gaussian
 
-    cl_samples_EE_PCl[map_num - 1] = cl_EE_PCl
-    cl_samples_BB_PCl[map_num - 1] = cl_BB_PCl
+    # Load the Gaussian PCl power spectrum and store into PCl data vectors
+    cl_EE_PCl_gaussian = np.loadtxt(f'{folder_path_gaussian}/Map{map_num}_Cl_EE_PCl.dat')
+    cl_BB_PCl_gaussian = np.loadtxt(f'{folder_path_gaussian}/Map{map_num}_Cl_BB_PCl.dat')
 
-# Compute the average of our Cl values for QML
-cl_avg_EE_QML = np.mean(cl_samples_EE_QML, axis=0)[0: n_ell]
-cl_avg_BB_QML = np.mean(cl_samples_BB_QML, axis=0)[0: n_ell]
+    cl_EE_PCl_gaussian = bins.bin_vector(cl_EE_PCl_gaussian)
+    cl_BB_PCl_gaussian = bins.bin_vector(cl_BB_PCl_gaussian)
+
+    cl_samples_EE_PCl_gaussian[map_num] = cl_EE_PCl_gaussian
+    cl_samples_BB_PCl_gaussian[map_num] = cl_BB_PCl_gaussian
+
+    # Load the log-normal QML power spectrum and store into QML data vectors
+    cl_EE_QML_lognormal = np.loadtxt(f'{folder_path_lognormal}/Map{map_num}_Cl_EE_QML.dat')
+    cl_BB_QML_lognormal = np.loadtxt(f'{folder_path_lognormal}/Map{map_num}_Cl_BB_QML.dat')
+
+    cl_EE_QML_lognormal = bins.bin_vector(cl_EE_QML_lognormal)
+    cl_BB_QML_lognormal = bins.bin_vector(cl_BB_QML_lognormal)
+
+    cl_samples_EE_QML_lognormal[map_num] = cl_EE_QML_lognormal
+    cl_samples_BB_QML_lognormal[map_num] = cl_BB_QML_lognormal
+
+    # Load the log-normal PCl power spectrum and store into PCl data vectors
+    cl_EE_PCl_lognormal = np.loadtxt(f'{folder_path_lognormal}/Map{map_num}_Cl_EE_PCl.dat')
+    cl_BB_PCl_lognormal = np.loadtxt(f'{folder_path_lognormal}/Map{map_num}_Cl_BB_PCl.dat')
+
+    cl_EE_PCl_lognormal = bins.bin_vector(cl_EE_PCl_lognormal)
+    cl_BB_PCl_lognormal = bins.bin_vector(cl_BB_PCl_lognormal)
+
+    cl_samples_EE_PCl_lognormal[map_num] = cl_EE_PCl_lognormal
+    cl_samples_BB_PCl_lognormal[map_num] = cl_BB_PCl_lognormal
+
+# Compute the average of our Cl values for QML - Gaussian
+cl_avg_EE_QML_gaussian = np.mean(cl_samples_EE_QML_gaussian, axis=0)
+cl_avg_BB_QML_gaussian = np.mean(cl_samples_BB_QML_gaussian, axis=0)
 
 # Compute PCl average
-cl_avg_EE_PCl = np.mean(cl_samples_EE_PCl, axis=0)[0: n_ell]
-cl_avg_BB_PCl = np.mean(cl_samples_BB_PCl, axis=0)[0: n_ell]
+cl_avg_EE_PCl_gaussian = np.mean(cl_samples_EE_PCl_gaussian, axis=0)
+cl_avg_BB_PCl_gaussian = np.mean(cl_samples_BB_PCl_gaussian, axis=0)
 
 # Compute the standard deviation of the QML Cl values
-cl_std_EE_QML = np.std(cl_samples_EE_QML, axis=0)[0: n_ell]
-cl_std_BB_QML = np.std(cl_samples_BB_QML, axis=0)[0: n_ell]
+cl_std_EE_QML_gaussian = np.std(cl_samples_EE_QML_gaussian, axis=0)
+cl_std_BB_QML_gaussian = np.std(cl_samples_BB_QML_gaussian, axis=0)
 
 # Compute the standard deviation of the PCl Cl values
-cl_std_EE_PCl = np.std(cl_samples_EE_PCl, axis=0)[0: n_ell]
-cl_std_BB_PCl = np.std(cl_samples_BB_PCl, axis=0)[0: n_ell]
+cl_std_EE_PCl_gaussian = np.std(cl_samples_EE_PCl_gaussian, axis=0)
+cl_std_BB_PCl_gaussian = np.std(cl_samples_BB_PCl_gaussian, axis=0)
+
+# Compute averages & st-dev's for log-normal case
+cl_avg_EE_QML_lognormal = np.mean(cl_samples_EE_QML_lognormal, axis=0)
+cl_avg_BB_QML_lognormal = np.mean(cl_samples_BB_QML_lognormal, axis=0)
+
+# Compute PCl average
+cl_avg_EE_PCl_lognormal = np.mean(cl_samples_EE_PCl_lognormal, axis=0)
+cl_avg_BB_PCl_lognormal = np.mean(cl_samples_BB_PCl_lognormal, axis=0)
+
+# Compute the standard deviation of the QML Cl values
+cl_std_EE_QML_lognormal = np.std(cl_samples_EE_QML_lognormal, axis=0)
+cl_std_BB_QML_lognormal = np.std(cl_samples_BB_QML_lognormal, axis=0)
+
+# Compute the standard deviation of the PCl Cl values
+cl_std_EE_PCl_lognormal = np.std(cl_samples_EE_PCl_lognormal, axis=0)
+cl_std_BB_PCl_lognormal = np.std(cl_samples_BB_PCl_lognormal, axis=0)
 
 # * Now plots!
 
 # Plot the average EE values
 plt.figure(figsize=(11, 6))
 
-plt.plot(ells, cl_avg_EE_QML, lw=2, c='cornflowerblue', label='QML')
-plt.errorbar(ells, cl_avg_EE_QML + theory_cl_noise, cl_std_EE_QML, linestyle='', c='cornflowerblue')
+plt.loglog(bins.bin_centres, cl_avg_EE_QML_gaussian, lw=2, c='cornflowerblue', label='QML Gaussian')
+plt.loglog(bins.bin_centres, cl_avg_EE_QML_lognormal, lw=2, c='purple', label='QML log-normal')
 
-plt.plot(ells, cl_avg_EE_PCl, lw=2, c='mediumseagreen', label='PCl')
-plt.errorbar(ells + 0.125, cl_avg_EE_PCl + theory_cl_noise, cl_std_EE_PCl, linestyle='', c='mediumseagreen')
+plt.loglog(bins.bin_centres, cl_avg_EE_PCl_gaussian, lw=2, c='lightseagreen', label='PCl Gaussian')
+plt.loglog(bins.bin_centres, cl_avg_EE_PCl_lognormal, lw=2, c='darkseagreen', label='PCl log-normal')
 
-plt.plot(ells, cl_th_EE + theory_cl_noise, ls='--', c='purple', label='Theory')
+plt.loglog(ells, cl_th_EE + theory_cl_noise, ls='--', c='cyan', label='Theory')
+plt.loglog(bins.bin_centres, cl_th_EE_binned + theory_cl_noise, ls='--', c='yellow', label='Theory')
 
 plt.xlabel(r'$\ell$')
 plt.ylabel(r'$C_{\ell}^{EE}$')
 
-plt.legend()
+plt.xlim(right=2 * n_side)
+plt.legend(ncol=3)
 plt.tight_layout()
 
-plt.savefig(f'{plots_folder}/Cl_EE_avg.pdf')
+plt.savefig(f'{plots_folder}/Cl_EE_avg_ells{ells_per_bin}.pdf')
 
 # Plot the average BB values
 plt.figure(figsize=(11, 6))
 
-plt.plot(ells, cl_avg_BB_QML, lw=2, c='cornflowerblue', label='QML')
-# plt.errorbar(ells, cl_avg_BB_QML, cl_std_BB_QML, linestyle='', c='cornflowerblue')
+plt.loglog(bins.bin_centres, cl_avg_BB_QML_gaussian, lw=2, c='cornflowerblue', label='QML Gaussian')
+plt.loglog(bins.bin_centres, cl_avg_BB_QML_lognormal, lw=2, c='purple', label='QML log-normal')
 
-plt.plot(ells, cl_avg_BB_PCl, lw=2, c='mediumseagreen', label='PCl')
-# plt.errorbar(ells+0.125, cl_avg_BB_PCl, cl_std_BB_PCl, linestyle='', c='mediumseagreen')
+plt.loglog(bins.bin_centres, cl_avg_BB_PCl_gaussian, lw=2, c='lightseagreen', label='PCl Gaussian')
+plt.loglog(bins.bin_centres, cl_avg_BB_PCl_lognormal, lw=2, c='darkseagreen', label='PCl log-normal')
 
-plt.plot(ells, [theory_cl_noise] * n_ell, lw=2, ls='--', c='purple', label='Theory')
+plt.loglog(ells, [theory_cl_noise] * n_ell, lw=2, ls='--', c='cyan', label='Theory')
 
 plt.xlabel(r'$\ell$')
 plt.ylabel(r'$C_{\ell}^{BB}$')
 
-plt.legend()
+plt.xlim(right=2 * n_side)
+plt.legend(ncol=3)
 plt.tight_layout()
 
-plt.savefig(f'{plots_folder}/Cl_BB_avg.pdf')
+plt.savefig(f'{plots_folder}/Cl_BB_avg_ells{ells_per_bin}.pdf')
 
 # EE average ratio
 plt.figure(figsize=(11, 6))
 
-plt.plot(ells, cl_avg_EE_QML / (cl_th_EE + theory_cl_noise), lw=2, c='cornflowerblue', label='QML')
+plt.semilogx(bins.bin_centres, cl_avg_EE_QML_gaussian / (cl_th_EE_binned + theory_cl_noise), lw=2, c='cornflowerblue', label='QML Gaussian')
+plt.semilogx(bins.bin_centres, cl_avg_EE_QML_lognormal / (cl_th_EE_binned + theory_cl_noise), lw=2, c='purple', label='QML log-normal')
 
-plt.plot(ells, cl_avg_EE_PCl / (cl_th_EE + theory_cl_noise), lw=2, c='mediumseagreen', label='PCl')
+plt.semilogx(bins.bin_centres, cl_avg_EE_PCl_gaussian / (cl_th_EE_binned + theory_cl_noise), lw=2, c='lightseagreen', label='PCl Gaussian')
+plt.semilogx(bins.bin_centres, cl_avg_EE_PCl_lognormal / (cl_th_EE_binned + theory_cl_noise), lw=2, c='darkseagreen', label='PCl log-normal')
 
-plt.plot(ells, pix_win, lw=2, c='crimson', label='Pix win')
+plt.semilogx(ells, pix_win, lw=2, c='crimson', label='Pix win')
 
 plt.xlabel(r'$\ell$')
 plt.ylabel(r'$C_{\ell}^{EE} / \hat{C}_{\ell}^{EE}$')
-plt.title('Ratio of recovered power spectra to theory values - Downgraded from $N=1024$ to $N=256$')
+plt.title(f'Ratio of recovered $EE$ power spectra to theory values - Downgraded from $N=1024$ to $N=256$, $\ell_{{b}} = {ells_per_bin}$')
 
-plt.legend()
+plt.xlim(right=2 * n_side)
+plt.legend(ncol=3)
 plt.tight_layout()
 
-plt.savefig(f'{plots_folder}/Cl_EE_avg_ratio.pdf')
+plt.savefig(f'{plots_folder}/Cl_EE_avg_ratio_ells{ells_per_bin}.pdf')
+
+# BB average ratio
+plt.figure(figsize=(11, 6))
+
+plt.semilogx(bins.bin_centres, cl_avg_BB_QML_gaussian / theory_cl_noise, lw=2, c='cornflowerblue', label='QML Gaussian')
+plt.semilogx(bins.bin_centres, cl_avg_BB_QML_lognormal / theory_cl_noise, lw=2, c='purple', label='QML log-normal')
+
+plt.semilogx(bins.bin_centres, cl_avg_BB_PCl_gaussian / theory_cl_noise, lw=2, c='lightseagreen', label='PCl Gaussian')
+plt.semilogx(bins.bin_centres, cl_avg_BB_PCl_lognormal / theory_cl_noise, lw=2, c='darkseagreen', label='PCl log-normal')
+
+plt.xlabel(r'$\ell$')
+plt.ylabel(r'$C_{\ell}^{BB} / \hat{C}_{\ell}^{BB}$')
+plt.title(f'Ratio of recovered $BB$ power spectra to theory values - Downgraded from $N=1024$ to $N=256$, $\ell_{{b}} = {ells_per_bin}$')
+
+plt.xlim(right=2 * n_side)
+plt.legend(ncol=2)
+plt.tight_layout()
+
+plt.savefig(f'{plots_folder}/Cl_BB_avg_ratio_ells{ells_per_bin}.pdf')
 
 # EE std-dev's
 plt.figure(figsize=(11, 6))
 
-plt.loglog(ells, cl_std_EE_QML, lw=2, c='cornflowerblue', label='QML')
-plt.loglog(ells, cl_std_EE_PCl, lw=2, c='mediumseagreen', label='PCl')
+plt.loglog(bins.bin_centres, cl_std_EE_QML_gaussian, lw=2, c='cornflowerblue', label='QML Gaussian')
+plt.loglog(bins.bin_centres, cl_std_EE_QML_lognormal, lw=2, c='purple', label='QML log-normal')
+
+plt.loglog(bins.bin_centres, cl_std_EE_PCl_gaussian, lw=2, c='lightseagreen', label='PCl Gaussian')
+plt.loglog(bins.bin_centres, cl_std_EE_PCl_lognormal, lw=2, c='darkseagreen', label='PCl log-normal')
 
 plt.loglog(ells, np.sqrt(np.diag(QML_fisher_matrix_inv[0: n_ell, 0: n_ell])), lw=2, ls='--', c='orange',
            label='inv-Fisher')
@@ -169,41 +260,54 @@ plt.loglog(ells, np.sqrt(np.diag(QML_fisher_matrix_inv[0: n_ell, 0: n_ell])), lw
 plt.xlabel(r'$\ell$')
 plt.ylabel(r'$\sigma_{C_{\ell}}^{EE}$')
 
-plt.legend()
+plt.xlim(right=2 * n_side)
+plt.legend(ncol=3)
 plt.tight_layout()
 
-plt.savefig(f'{plots_folder}/Cl_EE_std.pdf')
+plt.savefig(f'{plots_folder}/Cl_EE_std_ells{ells_per_bin}.pdf')
 
 plt.figure(figsize=(11, 6))
 
-plt.loglog(ells, cl_std_BB_QML, lw=2, c='cornflowerblue', label='QML')
-plt.loglog(ells, cl_std_BB_PCl, lw=2, c='mediumseagreen', label='PCl')
+plt.loglog(bins.bin_centres, cl_std_BB_QML_gaussian, lw=2, c='cornflowerblue', label='QML Gaussian')
+plt.loglog(bins.bin_centres, cl_std_BB_QML_lognormal, lw=2, c='purple', label='QML log-normal')
+
+plt.loglog(bins.bin_centres, cl_std_BB_PCl_gaussian, lw=2, c='lightseagreen', label='PCl Gaussian')
+plt.loglog(bins.bin_centres, cl_std_BB_PCl_lognormal, lw=2, c='darkseagreen', label='PCl log-normal')
 
 plt.loglog(ells,
-           np.sqrt(np.diag(QML_fisher_matrix_inv[n_ell_full: n_ell_full + n_ell, n_ell_full: n_ell_full + n_ell])),
+           np.sqrt(np.diag(QML_fisher_matrix_inv[n_ell: 2 * n_ell, n_ell: 2 * n_ell])),
            lw=2, ls='--', c='orange', label='inv-Fisher')
 
 plt.xlabel(r'$\ell$')
 plt.ylabel(r'$\sigma_{C_{\ell}}^{BB}$')
 
-plt.legend()
+plt.xlim(right=2 * n_side)
+plt.legend(ncol=3)
 plt.tight_layout()
 
-plt.savefig(f'{plots_folder}/Cl_BB_std.pdf')
+plt.savefig(f'{plots_folder}/Cl_BB_std_ells{ells_per_bin}.pdf')
 
 # Now plot the EE and BB std-dev ratio
-plt.figure(figsize=(11, 6))
+sns.reset_orig()
+mpl.rc_file_defaults()
+plt.rcParams['text.usetex'] = True
 
-plt.loglog(ells, cl_std_EE_PCl / cl_std_EE_QML, lw=2, c='cornflowerblue', label='$EE$')
-plt.loglog(ells, cl_std_BB_PCl / cl_std_BB_QML, lw=2, c='mediumseagreen', label='$BB$')
+plt.figure(figsize=(6, 3.5))
+
+plt.semilogx(bins.bin_centres, cl_std_EE_PCl_gaussian / cl_std_EE_QML_gaussian, lw=1.5, c='cornflowerblue', label='$EE$ Gaussian')
+plt.semilogx(bins.bin_centres, cl_std_EE_PCl_lognormal / cl_std_EE_QML_lognormal, lw=1.5, ls='--', c='purple', label='$EE$ Lognormal')
+
+plt.semilogx(bins.bin_centres, cl_std_BB_PCl_gaussian / cl_std_BB_QML_gaussian, lw=1.5, c='lightseagreen', label='$BB$ Gaussian')
+plt.semilogx(bins.bin_centres, cl_std_BB_PCl_lognormal / cl_std_BB_QML_lognormal, lw=1.5, ls='--', c='darkseagreen', label='$BB$ Lognormal')
 
 plt.xlabel(r'$\ell$')
 plt.ylabel(r'$\sigma_{C_{\ell}}^{\textrm{PCl}} / \sigma_{C_{\ell}}^{\textrm{QML}}$')
-plt.title('Ratio of power spectrum standard deviations for Log-Normal maps')
+# plt.title(f'Ratio of power spectrum standard deviations - Downgraded from $N=1024$ to $N=256$, $\ell_{{b}} = {ells_per_bin}$')
 
-plt.legend()
+plt.xlim(right=2 * n_side)
+plt.legend(ncol=2)
 plt.tight_layout()
 
-plt.savefig(f'{plots_folder}/Cl_std_ratio.pdf')
+plt.savefig(f'{plots_folder}/Cl_std_ratio_ells{ells_per_bin}.pdf')
 
 plt.close('all')
